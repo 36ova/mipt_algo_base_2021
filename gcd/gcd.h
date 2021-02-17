3767d415f590a824daf0a09dd757255ac1212668
Leeda <lidiya.troeshestova@gmail.com>
2021-02-17 22:47:53 +0300

    It should be working (x2)

--- gcd/gcd.h
+++ gcd/gcd.h
@@ -5,9 +5,11 @@ template <class T>
 T Gcd(T a, T b) {
     while (a != b) {
         if (a < b) {
-            swap(a, b);
+            T temp = a;
+            a = b;
+            b = temp;
         }
-        a %= b;
+        a -= b;
     }
     return a;
 }
