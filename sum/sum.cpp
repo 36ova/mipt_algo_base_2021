0374a18ea35bad4cbdd80fdd33ff85074a3ea085
Leeda <lidiya.troeshestova@gmail.com>
2021-02-19 16:06:10 +0300

    removed duplicate directory

--- sum/sum.cpp
+++
@@ -1,6 +0,0 @@
-#include "sum.h"
-
-int64_t Sum(int64_t x, int64_t y) {
-    int64_t ans = x + y;
-    return ans;
-}
--- sum/sum.h
+++
@@ -1,8 +0,0 @@
-#ifndef SUM_SUM_H
-#define SUM_SUM_H
-
-#include <cstdint>
-
-int64_t Sum(int64_t x, int64_t y);
-
-#endif
