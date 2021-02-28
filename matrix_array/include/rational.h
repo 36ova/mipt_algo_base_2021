80cf8af356ee428e475779ab477dd8c2ad0167b8
Leeda <lidiya.troeshestova@gmail.com>
2021-02-28 17:20:49 +0300

    did i need to upload these 2 as well??

---
+++ matrix_array/include/rational.h
@@ -0,0 +1,56 @@
+#ifndef RATIONAL_RATIONAL_H
+#define RATIONAL_RATIONAL_H
+
+//#include <util/constants.h>
+#include <cstdlib>
+#include <istream>
+#include <ostream>
+
+class RationalDivisionByZero : public std::runtime_error {
+public:
+    RationalDivisionByZero() : std::runtime_error("RationalDivisionByZero") {
+    }
+};
+
+int gcd(int a, int b);
+
+class Rational {
+private:
+    int p;
+    int q;
+
+public:
+    Rational() : p(0), q(1){};
+    Rational(const int x) : p(x), q(1){};  // NOLINT
+    Rational(const int x, const int y);
+    void Reduce();
+    int GetNumerator() const;
+    int GetDenominator() const;
+    void SetNumerator(int x);
+    void SetDenominator(int y);
+    Rational& operator+=(const Rational& other);
+    Rational& operator-=(const Rational& other);
+    Rational& operator*=(const Rational& other);
+    Rational& operator/=(const Rational& other);
+    Rational operator+() const;
+    Rational operator-() const;
+    Rational& operator++();
+    Rational& operator--();
+    Rational operator++(int);
+    Rational operator--(int);
+    friend std::istream& operator>>(std::istream& is, Rational& value);
+    friend std::ostream& operator<<(std::ostream& os, const Rational& value);
+};
+
+Rational operator+(const Rational& x, const Rational& y);
+Rational operator-(const Rational& x, const Rational& y);
+Rational operator*(const Rational& x, const Rational& y);
+Rational operator/(const Rational& x, const Rational& y);
+bool operator<(const Rational& x, const Rational& y);
+bool operator>(const Rational& x, const Rational& y);
+bool operator<=(const Rational& x, const Rational& y);
+bool operator>=(const Rational& x, const Rational& y);
+bool operator==(const Rational& x, const Rational& y);
+bool operator!=(const Rational& x, const Rational& y);
+
+#endif  // RATIONAL_RATIONAL_H
